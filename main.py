3afec5d32af85205464d563efd58ffc9e6024104
Леонид Соловьев <solovievleon@gmail.com>
2022-11-04 19:44:23 +0300

    work version

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def choice_char_class() -> str:
 
 
 def main() -> str:
-        run_screensaver()
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
